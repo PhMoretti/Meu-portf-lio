f896ced296cee5b9a54374bc4d96a84f52114cd6
pedro <morettipinsan@gmail.com>
2022-07-19 18:28:51 -0300

    Sobre finalizado

--- src/components/sobre/sobre.js
+++ src/components/sobre/sobre.js
@@ -1,5 +1,6 @@
 import './sobre.css'
 import React from "react";
+import imgjs from './imgjs.png'
 
 const Sobre = () => {
     return(
@@ -7,10 +8,16 @@ const Sobre = () => {
             <div className='container'>
                 <div className='sobre_cont'>
                     <div className='sobre_titulo'>
-                        <h2>Quer me conhecer melhor?</h2>
+                        <h2>Conheça-me abaixo.</h2>
                         <div className='fora_bola'>
                             <div className='bola'></div>
                         </div>    
+                    </div>
+                    <div className='sobre_json'>
+                        <img src={imgjs} className='img_json'></img>
+                    </div>
+                    <div className='sobre_texto'>
+                        <p>Olá! Meu nome é Pedro Henrique Moretti Pinsan de Oliveira Silva. Sou um programador em início de carreira e espero adquirir muitos conhecimentos ao longo de minha jornada. Para tal, por exemplo, desenvolvi este website utilizando <i>HTML</i>, <i>CSS</i>, <i>JS</i> e <i>REACT JS</i> para poder expor toda minha trajetória e tecnologias dominadas a quem seja necessário. Logo abaixo, seguem as tecnologias nas quais possuo conhecimento:</p>
                     </div>
                 </div>
             </div>
